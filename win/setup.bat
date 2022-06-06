8ef1a7bc69d03f8e31430db36dcb8312b56b9aa4
Quentin Leveque <quentin.leveque@bluebotics.com>
2022-06-06 20:17:18 +0200

    update

--- win/setup.bat
+++ win/setup.bat
@@ -4,6 +4,11 @@ mklink "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\config.xlaunch" 
 mklink "%APPDATA%\Code\User\settings.json" "%USERPROFILE%\dotfiles\vscode\.config\Code\User\settings.json"
 mklink "%APPDATA%\Code\User\keybindings.json" "%USERPROFILE%\dotfiles\vscode\.config\Code\User\keybindings.json"
 
+path|find /i "%USERPROFILE%\bin" > nul || setx path "%path%;%USERPROFILE%\bin"
+
+mkdir %USERPROFILE%\bin
+echo dir %%*> %USERPROFILE%\bin\ls.bat
+
 cd %USERPROFILE%
 bash -e ./dotfiles/setup
 
