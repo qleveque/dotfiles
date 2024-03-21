966be0bf2e3c8b4384d28c1d50715f4de97cb2f0
Quentin Leveque <qleveque@hotmail.com>
2024-03-21 18:39:50 +0100

    improve lesskey

--- config/Dotfiles/lesskey.py
+++ config/Dotfiles/lesskey.py
@@ -1,16 +1,23 @@
+def _(s):
+    for c in r'\^+@':
+        s = s.replace(c, rf'\{c}')
+    return s
+
+COMMIT = _(r'commit \w{40}')
+LINE = _(r'@@ -\w+')
+FILE = _(r'--- a/(\w|/|\.|-| )+')
+INDEX = _(r'index \w{7}\.\.\w{7}')
+
+REMOVE_COLORS = r"sed -r 's/\\x1B\\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g'"
 RESET = r'/\ek\ek\n'
 COPY = r"tr -d '\\n'|c"
 NA = 'noaction'
-TMUX = 'xargs -I v tmux neww '
+DIFFTOOL = 'git difftool --trust-exit-code --no-prompt'
+XARGS = 'xargs bash -c '
+CUT=r"tr '\\n' ' '|cut -d ' ' -f"
 
-def read(pattern):
-    s = pattern.replace(r'\\K', '')
-    return rf"mawj?\^{s}\nmm{RESET}'a|mhead -1|grep -oP -- '{pattern}'"
-
-COMMIT = r'commit \\K\\w{40}'
-LINE = r'\@\@ -\\K\\w\+'
-FILE = r'--- a/\\K(\\w|/|\\.|-| )\+'
-INDEX = r'index \\K\\w{7}\\.\\.\\w{7} '
+def read(pattern, n = 1):
+    return rf"mawj?\^{pattern}\nmm{RESET}'a|mhead -{n}|{REMOVE_COLORS}"
 
 print(rf'''#env
 LESS = -irFRX --mouse --wheel-lines=1
@@ -18,9 +25,9 @@ LESS = -irFRX --mouse --wheel-lines=1
 \e clear-search
 gg goto-line
 # Git
-yc {NA} {read(COMMIT)}|{COPY}\n
-yf {NA} {read(FILE)}|{COPY}\n
-yl {NA} {read(LINE)}|{COPY}\n
-gc {NA} {read(COMMIT)}|{TMUX}"tig show v"\n
-gf {NA} {read(INDEX)}|{TMUX}"git difftool --trust-exit-code --no-prompt v"\n
+yc {NA} {read(COMMIT)}|{CUT}2|{COPY}\n
+yf {NA} {read(FILE)}|cut -c 7-|{COPY}\n
+yl {NA} {read(LINE)}|grep -o '[0-9]\\+'|{CUT}1|{COPY}\n
+gc {NA} {read(COMMIT)}|{CUT}2|{XARGS}'tmux neww "tig show $0"'\n
+gf {NA} {read(INDEX, 2)}|{CUT}2,5|{XARGS}'tmux neww -e FILE="$PWD/${{1:2}}" "{DIFFTOOL} ${{0}}"'\n
 ''')
