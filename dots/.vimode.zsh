5fe596bc95ad9cd91f2325d6e692b94444782dc2
Quentin Leveque <qleveque@hotmail.com>
2024-10-03 08:35:37 +0200

    fix remap shift-space

--- dots/.vimode.zsh
+++ dots/.vimode.zsh
@@ -18,10 +18,10 @@ vm v visual-line-mode
 vm P visual-swap
 vm p visual-put
 im '^[[Z' forward-word
+im '^[[27;2;32~' insert-space
 im '^V' vi-put-before
 im '^I' fzf_completion
 im '^?' backward-delete-char
-im '^@' g-expansion
 im '^N' fzf-cd-widget
 im '^R' fzf-history-widget
 im '^P' goto-recent
@@ -37,6 +37,7 @@ set-cursor(){zvs-zle-keymap-select;local c=2;[[ ${KEYMAP} == main ]]&&c=6;printf
 goto-recent(){cd "$(eval ${FZF_P_COMMAND}|fzf --exact)";zle reset-prompt;zle zle-line-init}
 open-fm(){cd "$(vifm -c :only --choose-dir - . < /dev/tty)";zle reset-prompt; zle zle-line-init}
 open-gitw(){local k; read -k1 k; gitw $k ${PWD} < /dev/tty; zle reset-prompt; zle zle-line-init}
+insert-space() { LBUFFER+=" " }
 quit(){exit}
 vi-cut(){zle .vi-delete; printf '%s' "${CUTBUFFER}"|cb copy}
 vi-yank(){zle .vi-yank;zle set-mark-command -n -1;printf '%s' "${CUTBUFFER}"|cb copy}
@@ -46,8 +47,7 @@ opp-wrap(){read -k1 k;case $k in s)zle add-surround;;*)zle -U $k&&zle .vi-${WIDG
 visual-swap(){zle vi-delete;local b="${CUTBUFFER}";zle vi-put-before;printf '%s' "${b}"|cb copy}
 visual-put(){zle vi-delete; zle vi-put-before}
 copy-mode(){wez copy}
-g-expansion(){zle _mark_expansion && zle autosuggest-clear}
 wids=(goto-recent open-{fm,gitw} quit select-{bracketed,quoted} visual-{put,swap}
-      vi-{cut,yank,put-{before,after}} copy-mode g-expansion add-surround:surround
+      vi-{cut,yank,put-{before,after}} copy-mode insert-space add-surround:surround
       {change,delete}-wrap:opp-wrap zle-{keymap-select,line-{init,finish}}:set-cursor)
 for wid in "${wids[@]}"; do zle -N ${=wid//:/ }; done
