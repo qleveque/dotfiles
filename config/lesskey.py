0f74e3f312e6f2c1d41aed3a0466285713b8a974
Quentin Leveque <qleveque@hotmail.com>
2025-03-19 12:51:54 +0100

    update

--- config/lesskey.py
+++ config/lesskey.py
@@ -10,20 +10,26 @@ RUN = 'xargs bash -c '
 CUT = "cut -d' ' -f"
 
 COMMIT = r'\^commit \\w{40}'
-READ_COMMIT = f"{r(COMMIT)}|{CUT}2"
-IDX = r'\^index \\S*\\w{7}\\.\\.\\w{7}'
+STASH = r'\^stash@\\{[0-9]\+\\}: '
+REFLOG = r'\^\\w{7,} (\\(.*\\) )?HEAD@\\{[0-9]\+\\}: '
+GRAPH_COMMIT = r'\^[\\|\\* ]+ \\w{7,} - '
+ANY_COMMIT = f"{COMMIT}|{STASH}|{REFLOG}|{GRAPH_COMMIT}"
+READ_COMMIT_LINE = f"{r(ANY_COMMIT)}"
+IDX = r'\^index \\S*\\w{7,}\\.\\.\\w{7,}'
 READ_IDX_FILE = fr"{r(IDX, 3)}|grep -v ' /'|cut -c7-|{RUN}'echo $0 ${{@:$\#}}'"
 
 print(rf'''#env
-LESS = -irFRX --mouse --wheel-lines=1
+LESS = -irR --mouse --wheel-lines=1 +k
 #command
 gg goto-line
 zz back-scroll
 ^a {NA} mm|m{REMOVE_COLORS}|nvim -c 'syn off' -c 'set ls=0' -c 'set nonu'\n
+j forw-line-force
+yy {NA} ma|ahead -1|cb copy\n
 # Git
-gc {NA} {READ_COMMIT}|{RUN}'wez_wrap new git-commit "tig show $0"'\n
-gd {NA} {READ_IDX_FILE}|{RUN}'git_wrap i -i $0'\n
-yc {NA} {READ_COMMIT}|cb copy\n
+\n {NA} {READ_COMMIT_LINE}|git_wrap commit-line\n
+gd {NA} {READ_IDX_FILE}|git_wrap index\n
+yc {NA} {READ_COMMIT_LINE}|git_wrap commit-line -c\n
 yf {NA} {READ_IDX_FILE}|{CUT}2|cb copy\n
 c {NA} J/{COMMIT}\nma{RESET}'a
 C {NA} /{COMMIT}\nNma{RESET}'a
