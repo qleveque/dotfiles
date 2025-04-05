3e0a727af66059c7dd8657579deb0bd51c6a16bf
Quentin Leveque <qleveque@hotmail.com>
2025-04-05 10:42:29 +0200

    update

--- config/lesskey.py
+++ config/lesskey.py
@@ -1,9 +1,10 @@
 def r(pattern, n='1'):
     """find pattern and return n lines"""
-    return rf"maJ\eb?{pattern}\nmm{RESET}'a|m^Phead -n {n}|{REMOVE_COLORS}"
+    return rf"JmaJK\eb?{pattern}\n|a^P head -n {n}|{REMOVE_COLORS}"
 
-
-RESET = r'/\ek\ek\n'
+RESET_SEARCH = r'/^A^A\n'
+START = 'mo'
+END = rf'{RESET_SEARCH}\'o\ema\emo'
 REMOVE_COLORS = r"sed -r 's/\\x1B\\[[0-9;]*[mGK]//g'"
 NA = 'noaction'
 RUN = 'xargs bash -c '
@@ -16,20 +17,23 @@ GRAPH_COMMIT = r'\^([\\|\\*] )+\\w{6} '
 ANY_COMMIT = f"{COMMIT}|{STASH}|{REFLOG}|{GRAPH_COMMIT}"
 
 print(rf'''#env
-LESS = -irR --mouse --wheel-lines=1 +k
+LESS = -irR --mouse --wheel-lines=1 +/\^
 #command
 gg goto-line
-G goto-end \e\40
-o back-screen-force
+G goto-end
 zz back-scroll
 j forw-line-force
-^a {NA} mm|m^P{REMOVE_COLORS}|nvim -c 'syn off' -c 'set ls=0' -c 'set nonu'\n
-yy {NA} ma|a^Phead -1|{REMOVE_COLORS}|cb copy\n
+^a {NA} |.^P{REMOVE_COLORS}|nvim -c 'syn off' -c 'set ls=0' -c 'set nonu'\n
+yy {NA} |.^Phead -1|{REMOVE_COLORS}|cb copy\n
 # Git
-l {NA} {r(ANY_COMMIT)}|git_wrap log-commit\n
-gd {NA} {r(COMMIT, '-0')}|git_wrap log-diff\n
-yc {NA} {r(ANY_COMMIT)}|git_wrap log-commit -c\n
-yf {NA} {r(COMMIT, '-0')}|git_wrap log-diff -c\n
-c {NA} J/{COMMIT}\nma{RESET}'a
-C {NA} \eb?{COMMIT}\nma{RESET}'a
+l {NA} {START}{r(ANY_COMMIT)}|git_wrap log-commit\n{END}
+gd {NA} {START}{r(COMMIT, '-1')}|git_wrap log-diff\n{END}
+yc {NA} {START}{r(ANY_COMMIT)}|git_wrap log-commit -c\n{END}
+yf {NA} {START}{r(COMMIT, '-1')}|git_wrap log-diff -c\n{END}
+c {NA} J/{COMMIT}\n{START}{END}
+C {NA} \eb?{COMMIT}\n{START}{END}
+\e\e {NA} $
+#line-edit
+\e abort
+^A up
 ''')
