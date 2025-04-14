4b3972ed7d28ff15eac98688b021057fdd6d9c81
Quentin Leveque <qleveque@hotmail.com>
2025-04-14 12:50:13 +0200

    update

--- config/lesskey.py
+++ config/lesskey.py
@@ -1,18 +1,18 @@
 RESET_SEARCH = r'mS/^A^A\n\'S\emS'
-START = 'mO'
-END = rf'{RESET_SEARCH}\'O\emO'
-REMOVE_COLORS = r"sed -r 's/\\x1B\\[[0-9;]*[mGK]//g'"
+SAVE = 'mO'
+RESTORE = rf'\'O\emO'
 NA = 'noaction'
 
-COMMIT = r'\^commit \\w{40}'
-STASH = r'\^stash@\\{[0-9]\+\\}: '
-REFLOG = r'\^\\w{6} (\\(.*\\) )?HEAD@\\{[0-9]\+\\}: '
-GRAPH_COMMIT = r'\^([\\|\\*] )+\\w{6} '
-ANY_COMMIT = f"{COMMIT}|{STASH}|{REFLOG}|{GRAPH_COMMIT}"
+commit_pattern = r'\^commit \\w{40}'
+stash_pattern = r'\^stash@\\{[0-9]\+\\}: '
+reflog_pattern = r'\^\\w{6} (\\(.*\\) )?HEAD@\\{[0-9]\+\\}: '
+graph_pattern = r'\^([\\|\\*] )+\\w{6} '
+COMMIT_PATTERN = f"{commit_pattern}|{stash_pattern}|{reflog_pattern}|{graph_pattern}"
 
-BUFFER = f"|.^P{REMOVE_COLORS}"
+remove_colors = r"sed -r 's/\\x1B\\[[0-9;]*[mGK]//g'"
+BUFFER = f"|.^P{remove_colors}"
 COMMIT = f"{BUFFER}|git_wrap log-commit"
-UNTIL_COMMIT_LINE = rf"JK\eb?{ANY_COMMIT}\n|O^P{REMOVE_COLORS}"
+UNTIL_COMMIT_LINE = rf"JK\eb?{COMMIT_PATTERN}\n|O^P{remove_colors}"
 
 print(rf'''#env
 LESS = -irR --mouse --wheel-lines=1 +/\^
@@ -26,15 +26,14 @@ zz back-scroll
 j forw-line-force
 ^a {NA} {BUFFER}|nvim -c 'syn off' -c 'set ls=0' -c 'set nonu'\n
 yy {NA} {BUFFER}|head -1|cb copy\n
-\e\e {NA} $
 ^q quit
 # Git
 l {NA} {COMMIT}|xargs gitrs show\n
 yc {NA} {COMMIT}|cb copy\n
 dd {NA} {COMMIT}|xargs stash drop\n
 R {NA} {COMMIT}|xargs -i git rebase -i {{}}\^\n
-gd {NA} {START}{UNTIL_COMMIT_LINE}|git_wrap log-diff\n{END}
-yf {NA} {START}{UNTIL_COMMIT_LINE}|git_wrap log-diff copy\n{END}
-c {NA} J/{ANY_COMMIT}\n{RESET_SEARCH}
-C {NA} \eb?{ANY_COMMIT}\n{RESET_SEARCH}
+gd {NA} {SAVE}{UNTIL_COMMIT_LINE}|git_wrap log-diff\n{RESET_SEARCH}{RESTORE}
+yf {NA} {SAVE}{UNTIL_COMMIT_LINE}|git_wrap log-diff copy\n{RESET_SEARCH}{RESTORE}
+c {NA} J/{COMMIT_PATTERN}\n{RESET_SEARCH}
+C {NA} \eb?{COMMIT_PATTERN}\n{RESET_SEARCH}
 ''')
