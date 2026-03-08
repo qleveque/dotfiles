25fb1824ccaff52b9618f59293ba2fb8ecc65f2b
Quentin Leveque <qleveque@hotmail.com>
2026-03-08 11:21:53 +0000

    update

--- windows/config/.twm/start.bat
+++ windows/config/.twm/start.bat
@@ -3,7 +3,10 @@ set "SCRIPT_DIR=%~dp0"
 start "" "%PROGRAMFILES%\AutoHotkey\AutoHotkeyU64.exe" "%SCRIPT_DIR%\hotkeys.ahk"
 start "" "%PROGRAMFILES%\AutoHotkey\AutoHotkeyU64.exe" "%SCRIPT_DIR%\hide-taskbar.ahk"
 start "" "%PROGRAMFILES%\YASB\yasb.exe"
-start "" "%APPDATA%\Programs\Zero Install\0install-win.exe" run https://appdownload.deepl.com/windows/0install/deepl.xml
+tasklist /FI "IMAGENAME eq DeepL.exe" | find /I "DeepL.exe" >nul
+if errorlevel 1 (
+    start "" "%APPDATA%\Programs\Zero Install\0install-win.exe" run https://appdownload.deepl.com/windows/0install/deepl.xml
+)
 tasklist /FI "IMAGENAME eq Flow.Launcher.exe" | find /I "Flow.Launcher.exe" >nul
 if errorlevel 1 (
     start "" "%LOCALAPPDATA%\FlowLauncher\Flow.Launcher.exe"
