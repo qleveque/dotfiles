37f30309f33d13f295fc09cd2964d307392a6538
Quentin Leveque <qleveque@hotmail.com>
2026-04-13 17:24:14 +0200

    update

--- windows/config/.bin/vifm.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-setlocal enabledelayedexpansion
-set CMD=wsl.exe zsh -c "wslpath -m `vifm -c :only --choose-dir - . . < /dev/tty`"
-for /f "delims=" %%i in ('!CMD!') do set RESULT=%%i
-endlocal & set RESULT=%RESULT%
-
-cd %RESULT%
